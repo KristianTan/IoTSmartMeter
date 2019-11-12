b9f892faa88bf01589f6d3eee05332685a0c8e0b
Kristian Tan <kristian.tan@ntlworld.com>
2019-11-12 09:44:27 +0000

    Adding RPi.GPIO package and setting some pins

--- app.py
+++ app.py
@@ -1,29 +1,38 @@
 from flask import Flask, render_template
 import datetime
+import RPi.GPIO as GPIO
 
 app = Flask(__name__)
 
+# Create a dictionary called pins to store the pin number, name, and pin state:
+pins = {
+    24: {'name': 'coffee maker', 'state': GPIO.LOW},
+    25: {'name': 'lamp', 'state': GPIO.LOW}
+}
+
+# Set each pin as an output and make it low:
+for pin in pins:
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, GPIO.LOW)
+
 
 @app.route("/")
-def hello():
-   now = datetime.datetime.now()
-   time_string = now.strftime("%Y-%m-%d %H:%M")
-   template_data = {
-      'title' : 'HELLO!',
-      'time': time_string
-      }
-   return render_template('main.html', **template_data)
+def main():
+    # For each pin, read the pin state and store it in the pins dictionary:
+    for pin in pins:
+        pins[pin]['state'] = GPIO.input(pin)
+    # Put the pin dictionary into the template data dictionary:
+    template_data = {
+        'pins': pins
+    }
+    # Pass the template data into the template main.html and return it to the user
+    return render_template('main.html', **template_data)
+
 
 @app.route('/button')
 def button():
     print("Button press")
 
-    now = datetime.datetime.now()
-    time_string = now.strftime("%Y-%m-%d %H:%M")
-    template_data = {
-        'title': 'HELLO!',
-        'time': time_string
-    }
-    return render_template('main.html', **template_data)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8090)
