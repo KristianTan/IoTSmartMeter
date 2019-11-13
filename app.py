e20e3e29f448d3e81b06d3cc15a1c8858ac70d6c
Kristian Tan <kristian.tan@ntlworld.com>
2019-11-13 18:28:05 +0000

    Fix not tracking usage

--- app.py
+++ app.py
@@ -36,9 +36,9 @@ class DailyUsage(db.Model):
 
 
 def get_todays_usage():
-    # records = DailyUsage.query.order_by(asc(DailyUsage.date)).all()
+    records = DailyUsage.query.order_by(asc(DailyUsage.date)).all()
     latest_entry = DailyUsage.query.order_by(asc(DailyUsage.date)).first()
-    print(latest_entry)
+    print(records)
     # latest_entry = db.session.query(DailyUsage).order_by(DailyUsage.date.asc()).first()
     if latest_entry:
         latest_entry_date = date(latest_entry.date.year, latest_entry.date.month, latest_entry.date.day)
