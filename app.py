8467fad4ff04b83aeba3bb9fc58bfb71cf08839a
Kristian Tan <45459850+KristianTan@users.noreply.github.com>
2019-11-13 17:23:45 +0000

    Inputs (#2)
    Added ability to add new device and change cost per kWh

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 db = SQLAlchemy(app)
+os.environ['cost_per_kWh'] = '0.1622'
 
 
 # TODO: Move this into daily_usage class file
@@ -34,81 +35,17 @@ class DailyUsage(db.Model):
         return '<DailyUsage %r, %r, %r>' % (self.id, self.date, self.kwhUsed)
 
 
-db.create_all()
-
-# TODO: be able to query db by date
-daily_total = 0
-latest_entry = db.session.query(DailyUsage).order_by(DailyUsage.id.desc()).first()
-if latest_entry:
-    latest_entry_date = date(latest_entry.date.year, latest_entry.date.month, latest_entry.date.day)
-    if latest_entry_date == datetime.today().date():
-        daily_total = format(latest_entry.kwhUsed, '.7f')
-
-todays_cost = format(float(daily_total) * 0.1622, '0.5f')
-
-# Create dictionary to store pin info
-pins = {
-    25: {'name': 'Light', 'state': GPIO.LOW, 'on_time': None, 'on_date': None, 'Wattage': 15}
-}
-
-
-# Setup each pin
-for pin in pins:
-    GPIO.setup(pin, GPIO.OUT)
-    GPIO.output(pin, GPIO.LOW)
-
-
-@app.route("/")
-def main():
-    # For each pin, read the pin state and store it in the pins dictionary:
-    for pin in pins:
-        pins[pin]['state'] = GPIO.input(pin)
-
-    # Set the template data for the HTML template
-    template_data = {
-        'pins': pins,
-        'daily_total': daily_total,
-        'todays_cost': todays_cost
-    }
-
-    return render_template('main.html', **template_data)
-
-
-@app.route("/toggle/<change_pin>")
-def toggle_pin(change_pin):
-    change_pin = int(change_pin)
-    device_name = pins[change_pin]['name']
-
-    # Toggle the selected pin
-    GPIO.output(change_pin, not GPIO.input(change_pin))
-
-    if GPIO.input(change_pin) == 0:
-        if pins[change_pin]['on_time'] is not None:
-            create_entry(change_pin)
-    else:
-        pins[change_pin]['on_time'] = datetime.now()
-        pins[change_pin]['on_date'] = date.today()
-
-    for pin in pins:
-        pins[pin]['state'] = GPIO.input(pin)
-
+def get_todays_usage():
     latest_entry = db.session.query(DailyUsage).order_by(DailyUsage.id.desc()).first()
     if latest_entry:
         latest_entry_date = date(latest_entry.date.year, latest_entry.date.month, latest_entry.date.day)
         if latest_entry_date == datetime.today().date():
-            daily_total = format(latest_entry.kwhUsed, '.7f')
-    else:
-        daily_total = 0
+            return format(latest_entry.kwhUsed, '.7f')
+    return 0
 
-    todays_cost = format(float(daily_total) * 0.1622, '0.5f')
 
-    template_data = {
-        'pins': pins,
-        'daily_total': daily_total,
-        'todays_cost': todays_cost
-    }
-
-    return render_template('main.html', **template_data)
+def get_todays_cost():
+    return format(float(get_todays_usage()) * float(os.environ['cost_per_kWh']), '0.5f')
 
 
 def create_entry(change_pin):
@@ -135,16 +72,128 @@ def create_entry(change_pin):
     pins[change_pin]['on_time'] = None
     pins[change_pin]['on_date'] = None
 
-@app.route('/handle_data', methods=['POST'])
-def handle_data():
-    projectpath = request.form['kWhprice']
-    print(projectpath)
+db.create_all()
+
+daily_total = get_todays_usage()
+todays_cost = get_todays_cost()
+
+# Create dictionary to store pin info
+pins = {
+    25: {'name': 'Light', 'state': GPIO.LOW, 'on_time': None, 'on_date': None, 'Wattage': 15},
+    8: {'name': None, 'state': GPIO.LOW, 'on_time': None, 'on_date': None, 'Wattage': 0},
+    7: {'name': None, 'state': GPIO.LOW, 'on_time': None, 'on_date': None, 'Wattage': 0},
+    12: {'name': None, 'state': GPIO.LOW, 'on_time': None, 'on_date': None, 'Wattage': 0}
+}
+
+
+# Setup each pin
+for pin in pins:
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, GPIO.LOW)
+
+
+@app.route("/")
+def main():
+    # For each pin, read the pin state and store it in the pins dictionary:
+    for pin in pins:
+        pins[pin]['state'] = GPIO.input(pin)
+
+    # Set the template data for the HTML template
     template_data = {
         'pins': pins,
         'daily_total': daily_total,
-        'todays_cost': todays_cost
+        'todays_cost': todays_cost,
+        'cost_per_kWh': os.environ['cost_per_kWh'],
+        'display_new_device_form': False,
+        'display_change_kWh': False
+    }
+
+    return render_template('main.html', **template_data)
+
+
+@app.route("/toggle/<change_pin>")
+def toggle_pin(change_pin):
+    change_pin = int(change_pin)
+    device_name = pins[change_pin]['name']
+
+    # Toggle the selected pin
+    GPIO.output(change_pin, not GPIO.input(change_pin))
+
+    if GPIO.input(change_pin) == 0:
+        if pins[change_pin]['on_time'] is not None:
+            create_entry(change_pin)
+    else:
+        pins[change_pin]['on_time'] = datetime.now()
+        pins[change_pin]['on_date'] = date.today()
+
+    for pin in pins:
+        pins[pin]['state'] = GPIO.input(pin)
+
+    template_data = {
+        'pins': pins,
+        'daily_total': get_todays_usage(),
+        'todays_cost': get_todays_cost(),
+        'cost_per_kWh': os.environ['cost_per_kWh']
+    }
+
+    return render_template('main.html', **template_data)
+
+
+@app.route('/handle_change_kWh', methods=['POST'])
+def handle_change_kWh():
+    new_price = request.form['kWhprice']
+    os.environ['cost_per_kWh'] = str(new_price)
+    template_data = {
+        'pins': pins,
+        'daily_total': daily_total,
+        'todays_cost': todays_cost,
+        'cost_per_kWh': os.environ['cost_per_kWh']
     }
     return render_template('main.html', **template_data)
+
+
+@app.route("/devices/add_device")
+def add_new_device():
+    template_data = {
+        'pins': pins,
+        'daily_total': daily_total,
+        'todays_cost': todays_cost,
+        'cost_per_kWh': os.environ['cost_per_kWh'],
+        'display_new_device_form': True
+    }
+    return render_template('main.html', **template_data)
+
+
+@app.route('/handle_new_device', methods=['POST'])
+def handle_new_device():
+    new_name = request.form['new_device_name']
+    new_wattage = float(request.form['new_device_wattage'])
+    for key in pins:
+        if pins[key]['name'] is None:
+            pins[key]['name'] = new_name
+            pins[key]['Wattage'] = new_wattage
+            break
+
+    template_data = {
+        'pins': pins,
+        'daily_total': daily_total,
+        'todays_cost': todays_cost,
+        'cost_per_kWh': os.environ['cost_per_kWh'],
+    }
+    return render_template('main.html', **template_data)
+
+
+@app.route("/update_info/kWh")
+def change_kWh():
+    template_data = {
+        'pins': pins,
+        'daily_total': daily_total,
+        'todays_cost': todays_cost,
+        'cost_per_kWh': os.environ['cost_per_kWh'],
+        'display_change_kWh': True
+    }
+    return render_template('main.html', **template_data)
+
 
 
 if __name__ == '__main__':
