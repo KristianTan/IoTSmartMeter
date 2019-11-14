8529b040e6b086ff29837ca0a3a73d9e71a84865
Kristian Tan <kristian.tan@ntlworld.com>
2019-11-14 10:35:54 +0000

    Display bars in correct order

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ def generate_graph_data():
         count += 1
         if count >= 5:
             break
-    return labels, values, max
+    return labels.reverse(), values.reverse(), max
 
 db.create_all()
 
