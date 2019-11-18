86e3f7337061472a13ce028e4a55da93e02a5aeb
Kristian Tan <kristian.tan@ntlworld.com>
2019-11-18 22:12:17 +0000

    test

--- services/daily_usage_service.py
+++ services/daily_usage_service.py
@@ -19,3 +19,13 @@ class DailyUsageService:
             if latest_entry_date == datetime.today().date():
                 return format(latest_entry.kwhUsed, '.7f')
         return 0
+
+class test:
+    def get_todays_usage(self):
+        latest_entry = DailyUsage.query.order_by(desc(DailyUsage.date)).first()
+        # latest_entry = db.session.query(DailyUsage).order_by(DailyUsage.date.asc()).first()
+        if latest_entry:
+            latest_entry_date = date(latest_entry.date.year, latest_entry.date.month, latest_entry.date.day)
+            if latest_entry_date == datetime.today().date():
+                return format(latest_entry.kwhUsed, '.7f')
+        return 0
