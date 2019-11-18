cc6fd590795361c38ff207982b396c90bef42787
Kristian Tan <kristian.tan@ntlworld.com>
2019-11-18 22:25:21 +0000

    See latest entry

--- app.py
+++ app.py
@@ -58,6 +58,7 @@ def create_entry(change_pin):
 
     # Formula to calculate kWh based on time and wattage
     kwh = pins[change_pin]['wattage'] * (elapsed / 3600) / 1000
+    print(latest_entry)
     # If there is already an entry for today, update on time
     if latest_entry:
         latest_entry_date = date(latest_entry.date.year, latest_entry.date.month, latest_entry.date.day)
