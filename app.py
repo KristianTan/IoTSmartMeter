064cabf67924babf72b266acdfbc6e7ead425808
Kristian Tan <45459850+KristianTan@users.noreply.github.com>
2019-11-18 22:41:24 +0000

    Issue (#5)
    Fixed database logic for creating new entries

--- app.py
+++ app.py
@@ -59,14 +59,15 @@ def create_entry(change_pin):
     # Formula to calculate kWh based on time and wattage
     kwh = pins[change_pin]['wattage'] * (elapsed / 3600) / 1000
     # If there is already an entry for today, update on time
-    if latest_entry:
-        latest_entry_date = date(latest_entry.date.year, latest_entry.date.month, latest_entry.date.day)
-        if latest_entry_date == start_date:
-            latest_entry.kwhUsed += kwh
-        else:
-            # If no entry for today, make one
-            entry = DailyUsage(date=start_date, kwhUsed=kwh)
-            db.session.add(entry)
+    print(latest_entry)
+    if latest_entry and date(latest_entry.date.year, latest_entry.date.month, latest_entry.date.day) == start_date:
+        # latest_entry_date = date(latest_entry.date.year, latest_entry.date.month, latest_entry.date.day)
+        # if latest_entry_date == start_date:
+        latest_entry.kwhUsed += kwh
+    else:
+        # If no entry for today, make one
+        entry = DailyUsage(date=start_date, kwhUsed=kwh)
+        db.session.add(entry)
     db.session.commit()
     pins[change_pin]['on_time'] = None
     pins[change_pin]['on_date'] = None
@@ -140,7 +141,6 @@ def main():
 @app.route("/toggle/<change_pin>")
 def toggle_pin(change_pin):
     change_pin = int(change_pin)
-    device_name = pins[change_pin]['name']
 
     # Toggle the selected pin
     GPIO.output(change_pin, not GPIO.input(change_pin))
