54c1174e44bca885acd0d5d16cc3ee8e6181e288
Kristian Tan <kristian.tan@ntlworld.com>
2019-11-18 21:43:48 +0000

    imports

--- services/daily_usage.py
+++ services/daily_usage.py
@@ -1,8 +1,7 @@
 from datetime import datetime, date
 
 from sqlalchemy import desc
-from app import DailyUsage
-
+import app
 
 class DailyUsageService:
 
@@ -12,7 +11,7 @@ class DailyUsageService:
 
     @staticmethod
     def get_todays_usage():
-        latest_entry = DailyUsage.query.order_by(desc(DailyUsage.date)).first()
+        latest_entry = app.DailyUsage.query.order_by(desc(DailyUsage.date)).first()
         # latest_entry = db.session.query(DailyUsage).order_by(DailyUsage.date.asc()).first()
         if latest_entry:
             latest_entry_date = date(latest_entry.date.year, latest_entry.date.month, latest_entry.date.day)
