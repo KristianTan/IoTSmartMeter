f657c7648b07ae8431c41b95617fce8b2175190e
Kristian Tan <kristian.tan@ntlworld.com>
2019-11-18 22:25:51 +0000

    msg

--- app.py
+++ app.py
@@ -61,6 +61,7 @@ def create_entry(change_pin):
     print(latest_entry)
     # If there is already an entry for today, update on time
     if latest_entry:
+        print("HERE?")
         latest_entry_date = date(latest_entry.date.year, latest_entry.date.month, latest_entry.date.day)
         if latest_entry_date == start_date:
             latest_entry.kwhUsed += kwh
